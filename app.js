27dbf528500cc28fa0a661b18a12bd4e60359a05
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2023-08-14 21:23:52 +0200

    added popup for POIs in Explore
    Popup data provided by Yelp

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var exploreRouter = require('./routes/explore');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/explore', exploreRouter);
+app.use('/api', apiRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/exploreMapview.js
+++ public/javascripts/exploreMapview.js
@@ -21,6 +21,18 @@ map.addControl(new mapboxgl.GeolocateControl({
     showUserHeading: true
 }));
 
+// YELP API
+
+// getBusinessDetails function
+async function getBusinessDetails(term, lon, lat) {
+    //make call to /api/business-details/lon/lat/term
+    const query = await fetch(`/api/business-details/${lon}/${lat}/${term}`, {method: 'GET'});
+    const data = await query.json();
+    console.log(data);
+    // return the data
+    return data;
+}
+
 // ISOCHROME API 
 
 // Create constants to use in getIso()
@@ -35,8 +47,62 @@ function outputPOIs(pois) {
     list.innerHTML = '';
     pois.forEach((poi) => {
         const item = document.createElement('li');
-        item.textContent = poi.properties.name;
+        let name = poi.properties.name;
+        item.textContent = name;
         item.classList.add('text-sm');
+        // add the lon and lat as data attributes
+        item.setAttribute('data-lon', poi.geometry.coordinates[0]);
+        item.setAttribute('data-lat', poi.geometry.coordinates[1]);
+        // add a click event listener to each item
+        item.addEventListener('click', async (event) => {
+            // get the name of the clicked item
+            const name = event.target.textContent;
+            // get the lon and lat from the clicked item
+            const lon = event.target.getAttribute('data-lon');
+            const lat = event.target.getAttribute('data-lat');
+            // set the map center to the clicked item
+            map.setCenter([lon, lat]);
+            // remove the marker
+            marker.remove();
+            // add a marker to the clicked item
+            marker.setLngLat([lon, lat]).addTo(map);
+            // call the businesses API
+            let data = await getBusinessDetails(name, lon, lat);
+
+            // try to remove the popup
+            try {
+                marker.getPopup().remove();
+            } catch (error) {
+                console.log(error);
+            }
+
+            // add a popup to the marker using the returned data from the businesses API
+            const popup = new mapboxgl.Popup({offset: 25}).setHTML(
+                `<img class="w-full h-32 object-cover overflow-hidden" src="${data.businesses[0].image_url}" alt="business image">
+                <h3 class="text-base underline">${data.businesses[0].name}</h3>
+                <div class="flex items-center">
+                    <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                    </svg>
+                    <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">${data.businesses[0].rating}</p>
+                    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                    <p class="text-sm font-medium text-gray-900 dark:text-white">${data.businesses[0].review_count} reviews</p>
+                </div>
+                <p class="text-sm">Closed: ${data.businesses[0].is_closed}</p>
+                
+                <p class="text-sm">Price: ${data.businesses[0].price}</p>
+                <p class="text-sm">Phone: ${data.businesses[0].phone}</p>
+                <p class="text-sm">Address: ${data.businesses[0].location.address1}, ${data.businesses[0].location.zip_code} ${data.businesses[0].location.city}</p>
+                <br>
+                <a class="text-sm" href="${data.businesses[0].url}" target="_blank">Visit Yelp Site</a>`
+            );
+
+            // add the popup to the marker
+            marker.setPopup(popup);
+            // open the popup
+            popup.addTo(map);
+        });
+
         list.appendChild(item);
     });
     // add it to the html
